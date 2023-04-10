07340aef5353327b528f11898c90103d9e119ebf
angikkate <angikkate@gmail.com>
2023-04-10 19:53:41 +0300

    Create localStorage.js

---
+++ src/js/localStorage.js
@@ -0,0 +1,32 @@
+const load = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    //return serializedState === null ? [] : JSON.parse(serializedState);
+    return (serializedState = JSON.parse(serializedState) || undefined);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+const save = (key, value) => { 
+    try { 
+      const serializedState = JSON.stringify(value);
+      localStorage.setItem(key, serializedState);
+    } catch (error) {
+      console.error('Set items error: ', error.message);
+    }
+};
+
+const remove = key => {
+  try {
+    localStorage.removeItem(key);
+  } catch (error) {
+    console.error('Remove state error: ', error.message);
+  }
+};
+
+export default {
+  load,  
+  save, 
+  remove,
+};
