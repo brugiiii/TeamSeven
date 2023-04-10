fefc627dc636e774f940b08152cccda377fd4bdb
unknown <edyabrug91@gmail.com>
2023-04-10 15:14:25 +0300

    done

--- src/js/renderCards.js
+++ src/js/renderCards.js
@@ -3,19 +3,22 @@ const newApiService = new NewApiService();
 
 newApiService.fetchPopularMovies().then(({ results }) => {
   results.map(result => {
-    const { poster_path, original_title, genre_ids, release_date } = result;
+    const { poster_path, original_title, genre_ids, release_date, id } = result;
 
     const mk = `
-    <div class="card-item">
+    <div class="card-item" data-action="${id}">
         <div class="card-img">
             <img src="https://image.tmdb.org/t/p/w500${poster_path}" class="card-img-top" alt="${original_title}">
         </div> 
         <div class="card-body">
-            <p class="card-title">${original_title}</p>
-            <p class="card-text">${genre_ids.join(", ")} | ${release_date.slice(0, 4)}</p>
+            <h2 class="card-title">${original_title}</h2>
+            <p class="card-text">${genre_ids.join(', ')} | ${release_date.slice(
+      0,
+      4
+    )}</p>
         </div>
     </div>
-    `
+    `;
     const container = document.querySelector('.main-content');
     container.insertAdjacentHTML('beforeend', mk);
   });
