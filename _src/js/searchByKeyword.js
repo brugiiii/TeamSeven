7444213d752d1bac0729d9224216c029064fc43a
Vladimir <86652590+Emersonnaw@users.noreply.github.com>
2023-04-11 21:11:55 +0300

    new braich

--- _src/js/searchByKeyword.js
+++ _src/js/searchByKeyword.js
@@ -1,7 +1,6 @@
 import apiServer from './api-servis';
 import searchRenderBox from '../templates/searchRenger.hbs'
 const apiServise = new apiServer();
-const numberOfGeneras = 4;
 const refs = {
     searchForm: document.querySelector('.header__search'),
     main: document.querySelector('.main-content'),
@@ -12,8 +11,7 @@ refs.searchForm.addEventListener('submit', onInputForm);
   function onInputForm(e){
     e.preventDefault();
     apiServise.query = e.currentTarget.elements.search.value;
-     ganreListProcessin().then(createCards);  
-
+    ganreListProcessin().then(createCards);   
 }
 
 async function ganreListProcessin(){
@@ -23,10 +21,14 @@ async function ganreListProcessin(){
 
     if(!localStorage.getItem('ganre-List')){
 
-       const  ganres =  await apiServise.fetchGenresMovies();     
-        comparisonList = ganres;
-         
-        const localStorageJson = JSON.stringify(ganres);
+       const  ganres =  await apiServise.fetchGenresMovies();
+
+       const ganreList = ganres.map(({id, name})=> {
+          return {[id]:name,} ;
+        });
+
+        comparisonList = ganreList;
+        const localStorageJson = JSON.stringify(ganreList);
         localStorage.setItem('ganre-List',localStorageJson);
     }
     
@@ -36,31 +38,13 @@ async function ganreListProcessin(){
   return comparisonList;
 }
 
-async function createCards(genresBase){
+async function createCards(array){
     try{
         await apiServise.fetchSearchMoviesPages().then(({results}) =>{
-          
             const articleStore = results.map(({title, release_date, poster_path, genre_ids, id}) =>{
                 release_date = release_date.slice(0,4);
-                poster_path = `https://image.tmdb.org/t/p/w500${poster_path}`;
-
-                const genres = [...genresBase.genres];
-
-                 const genreIds = genre_ids;
-                 let genresNames = [];
-                 const other ="Other"
-
-                for (let i = 0; i < genreIds.length; i++) {
-                  const genre = genres.find(g => g.id === genreIds[i]);
-                  genresNames.push(genre.name); 
-                }
-               let currentGanre = [...genresNames.slice(0,2), other].join(' ');
-
-                if( genresNames.length < numberOfGeneras ){
-                  currentGanre = [...genresNames].join(' ');
-                }
-                  
-               return {title, release_date, currentGanre, poster_path, id};
+                poster_path = `https://image.tmdb.org/t/p/w500${poster_path}`;             
+               return {title, release_date, poster_path, genre_ids, id};
             });
              const markup = searchRenderBox({articleStore});
              refs.main.innerHTML = markup;       
