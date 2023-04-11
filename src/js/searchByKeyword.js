38dfec67ebb91b16a164dd6f7fe64ac5402c5c67
Vladimir <86652590+Emersonnaw@users.noreply.github.com>
2023-04-11 17:36:19 +0300

    made the correct display of genres

--- src/js/searchByKeyword.js
+++ src/js/searchByKeyword.js
@@ -1,6 +1,7 @@
 import apiServer from './api-servis';
 import searchRenderBox from '../templates/searchRenger.hbs'
 const apiServise = new apiServer();
+const numberOfGeneras = 4;
 const refs = {
     searchForm: document.querySelector('.header__search'),
     main: document.querySelector('.main-content'),
@@ -38,13 +39,32 @@ async function ganreListProcessin(){
   return comparisonList;
 }
 
-async function createCards(array){
+async function createCards(genresBase){
     try{
         await apiServise.fetchSearchMoviesPages().then(({results}) =>{
+           
             const articleStore = results.map(({title, release_date, poster_path, genre_ids, id}) =>{
                 release_date = release_date.slice(0,4);
-                poster_path = `https://image.tmdb.org/t/p/w500${poster_path}`;             
-               return {title, release_date, poster_path, genre_ids, id};
+                poster_path = `https://image.tmdb.org/t/p/w500${poster_path}`;
+
+                const genres = [...genresBase.genres];
+
+                const genreIds = genre_ids;
+                let genresNames = [];
+                
+                const other ="Other"
+                for (let i = 0; i < genreIds.length; i++) {
+                  const genre = genres.find(g => g.id === genreIds[i]);
+                  genresNames.push(genre.name);
+                  
+                }
+               let currentGanre = [...genresNames.slice(0,2), other].join(' ');
+
+                if( genresNames.length < numberOfGeneras ){
+                  currentGanre = [...genresNames].join(' ');
+                }
+                  
+               return {title, release_date, poster_path, currentGanre, id};
             });
              const markup = searchRenderBox({articleStore});
              refs.main.innerHTML = markup;       
