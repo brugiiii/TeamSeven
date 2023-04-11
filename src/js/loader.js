62b1ca094d27fca877dc8fc791870c7e779d3680
Anna Kreidina <kav99mail@gmail.com>
2023-04-11 18:45:22 +0300

    add/loader/home

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,10 +1,25 @@
-const mainEl = document.querySelector('.main-content');
+const refs = {
+  mainEl: document.querySelector('.main-content'),
+};
 
-function showLoader() {
-  const markUpLoader = `<div class="preloader">
-  <div class='loader'></div>
-    </div>`;
-  mainEl.insertAdjacentHTML('afterbegin', markUpLoader);
+export default class NewLoader {
+  constructor() {
+    this.mainEl = refs.mainEl;
+    this.preloaderEl = null;
+  }
+
+  showLoader() {
+    const markUpLoader = `<div id='page-loader' class="preloader preloader__home">
+    <div class='loader loader__home'></div>
+  </div>`;
+    this.mainEl.insertAdjacentHTML('afterbegin', markUpLoader);
+
+    this.preloaderEl = document.querySelector('.preloader');
+  }
+  hideLoader() {
+    if (this.preloaderEl) {
+      this.preloaderEl.remove();
+      this.preloaderEl = null;
+    }
+  }
 }
-
-// showLoader();
