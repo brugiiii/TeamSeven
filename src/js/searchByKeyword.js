c1f0b293dd1cce395a6b45cbacf8a073eb2a8623
Vladimir <86652590+Emersonnaw@users.noreply.github.com>
2023-04-11 19:42:26 +0300

    repair version

--- src/js/searchByKeyword.js
+++ src/js/searchByKeyword.js
@@ -1,7 +1,7 @@
 import apiServer from './api-servis';
 import searchRenderBox from '../templates/searchRenger.hbs'
 const apiServise = new apiServer();
-const numberOfGeneras = 3;
+const numberOfGeneras = 4;
 const refs = {
     searchForm: document.querySelector('.header__search'),
     main: document.querySelector('.main-content'),
@@ -12,7 +12,8 @@ refs.searchForm.addEventListener('submit', onInputForm);
   function onInputForm(e){
     e.preventDefault();
     apiServise.query = e.currentTarget.elements.search.value;
-    ganreListProcessin().then(createCards);   
+     ganreListProcessin().then(createCards);  
+
 }
 
 async function ganreListProcessin(){
@@ -22,14 +23,10 @@ async function ganreListProcessin(){
 
     if(!localStorage.getItem('ganre-List')){
 
-       const  ganres =  await apiServise.fetchGenresMovies();
-
-       const ganreList = ganres.map(({id, name})=> {
-          return {[id]:name,} ;
-        });
-
-        comparisonList = ganreList;
-        const localStorageJson = JSON.stringify(ganreList);
+       const  ganres =  await apiServise.fetchGenresMovies();     
+        comparisonList = ganres;
+         
+        const localStorageJson = JSON.stringify(ganres);
         localStorage.setItem('ganre-List',localStorageJson);
     }
     
@@ -42,21 +39,20 @@ async function ganreListProcessin(){
 async function createCards(genresBase){
     try{
         await apiServise.fetchSearchMoviesPages().then(({results}) =>{
-           
+          
             const articleStore = results.map(({title, release_date, poster_path, genre_ids, id}) =>{
                 release_date = release_date.slice(0,4);
                 poster_path = `https://image.tmdb.org/t/p/w500${poster_path}`;
 
                 const genres = [...genresBase.genres];
 
-                const genreIds = genre_ids;
-                let genresNames = [];
-                
-                const other ="Other"
+                 const genreIds = genre_ids;
+                 let genresNames = [];
+                 const other ="Other"
+
                 for (let i = 0; i < genreIds.length; i++) {
                   const genre = genres.find(g => g.id === genreIds[i]);
-                  genresNames.push(genre.name);
-                  
+                  genresNames.push(genre.name); 
                 }
                let currentGanre = [...genresNames.slice(0,2), other].join(' ');
 
@@ -64,7 +60,7 @@ async function createCards(genresBase){
                   currentGanre = [...genresNames].join(' ');
                 }
                   
-               return {title, release_date, poster_path, currentGanre, id};
+               return {title, release_date, currentGanre, poster_path, id};
             });
              const markup = searchRenderBox({articleStore});
              refs.main.innerHTML = markup;       
