55027a90581d141fe81377b162921b444fc04ec7
Vladimir <86652590+Emersonnaw@users.noreply.github.com>
2023-04-12 23:03:43 +0300

    maked vision ganges on the default page when page open firsrt time

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,24 +1,10 @@
 const mainEl = document.querySelector('.main-content');
 
-export default class Loader {
-  constructor() {
-    this.mainEl = mainEl;
-    this.preloaderEl = null;
-  }
-
-  showLoader() {
-    const markUpLoader = `<div id='page-loader' class="preloader preloader__home">
-    <div class='loader loader__home'></div>
-  </div>`;
-    this.mainEl.insertAdjacentHTML('afterbegin', markUpLoader);
-
-    // showLoader();
-    this.preloaderEl = document.querySelector('.preloader');
-  }
-  hideLoader() {
-    if (this.preloaderEl) {
-      this.preloaderEl.remove();
-      this.preloaderEl = null;
-    }
-  }
+function showLoader() {
+  const markUpLoader = `<div class="preloader">
+  <div class='loader'></div>
+    </div>`;
+  mainEl.insertAdjacentHTML('afterbegin', markUpLoader);
 }
+
+// showLoader();
--- src/js/renderCards.js
+++ src/js/renderCards.js
@@ -1,20 +1,37 @@
-/* import cardTemplate from '../templates/cardTemplate.hbs';
+import cardTemplate from '../templates/cardTemplate.hbs';
 import NewApiService from './api-servis';
-import Loader from './loader';
-
-const loader = new Loader();
+import { numberOfGeneras ,ganreListProcessin} from './searchByKeyword'
 const newApiService = new NewApiService();
-
+let genresBase = null;
+ async function getBase() {
+  genresBase = await ganreListProcessin();
+ }
+ getBase();
+  
 newApiService.fetchPopularMovies().then(({ results }) => {
+
+ 
   results.map(result => {
     const { poster_path, original_title, genre_ids, release_date, id } = result;
-    const genres = genre_ids.join(', ');
+    //  const genres = genre_ids.join(', ');
     const date = release_date.slice(0, 4);
 
-    const mk = cardTemplate({ poster_path, original_title, genres, date, id });
+   const genres = [...genresBase.genres];
+   const genreIds = genre_ids;
+   let genresNames = [];
+   const other ="Other"
+   for (let i = 0; i < genreIds.length; i++) {
+    const genre = genres.find(g => g.id === genreIds[i]);
+    genresNames.push(genre.name); 
+  }
+  let currentGanre = [...genresNames.slice(0,2), other].join(' ');
+  if( genresNames.length < numberOfGeneras ){
+    currentGanre = [...genresNames].join(' ');
+  }
+
+    const mk = cardTemplate({ poster_path, original_title, currentGanre, date, id });
 
     const container = document.querySelector('.main-content');
     container.insertAdjacentHTML('beforeend', mk);
   });
 });
- */
