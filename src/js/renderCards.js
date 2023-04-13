8d988a8d55fc82960432272ff0aea0eaeb5eb98b
Vladimir <86652590+Emersonnaw@users.noreply.github.com>
2023-04-13 20:51:52 +0300

    fix oncorrect gangers

--- src/js/renderCards.js
+++ src/js/renderCards.js
@@ -1,4 +1,4 @@
-import cardTemplate from '../templates/cardTemplate.hbs';
+import cardTemplate from '../templates/cardTemplate.hbs'
 import NewApiService from './api-servis';
 import { numberOfGeneras, ganreListProcessin } from './searchByKeyword';
 import NewLoader from './loader';
@@ -12,43 +12,41 @@ async function fetchData(genresBase) {
   try {
     newLoader.showLoader();
     await new Promise(resolve => setTimeout(resolve, 300));
-    const mk =  await newApiService.fetchPopularMovies().then(({ results }) => {
-      results.map(result => {
-        const { poster_path, original_title, genre_ids, release_date, id } =
-          result;
+    await newApiService.fetchPopularMovies().then(({ results }) => {
+      const mk =  results.map(result => {
+        const { poster_path, original_title, genre_ids, release_date, id } = result;
         const date = release_date.slice(0, 4);
 
         const genres = [...genresBase.genres];
         const genreIds = genre_ids;
-
+       
         let genresNames = [];
         const other = 'Other';
         for (let i = 0; i < genreIds.length; i++) {
           const genre = genres.find(g => g.id === genreIds[i]);
           genresNames.push(genre.name);
         }
-        //let currentGanre = null;
-        if (genresNames.length < numberOfGeneras) {
-          // currentGanre = genresNames.join(',  ');
-        } else {
-          // currentGanre = [...genresNames.slice(0, 2), other].join(', ');
-        }
-        currentGanre = genresNames.join(', ');
-
-      return{
-          poster_path,
-          original_title,
-          currentGanre,
-          date,
-          id,
-        };
-
         
+        let currentGanre = null;
+        if (genresNames.length < numberOfGeneras) {
+              currentGanre = genresNames.join(',  ');
+          } else {
+           currentGanre = [...genresNames.slice(0, 2), other].join(', ');
+          }
+        
+
+      return{ poster_path, original_title, currentGanre, date,id };
+         
       });
+      const marKup = cardTemplate({mk});
+      console.log(marKup);
+       const container = document.querySelector('.main-content');
+        container.innerHTML = marKup;
+
+
     });
 
-    const container = document.querySelector('.main-content');
-        container.insertAdjacentHTML('beforeend', mk);
+    
     newLoader.hideLoader();
   } catch (error) {
     console.error('Помилка під час отримання даних:', error);
