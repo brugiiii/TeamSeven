2ea903fe28829596d8a9a83a2d9a10db1126b103
Eduard Bruhosh <edyabrug91@gmail.com>
2023-04-13 10:39:22 +0300

    bugfix

--- src/js/localStorage.js
+++ src/js/localStorage.js
@@ -1,24 +1,17 @@
-
-import {watchedBtnToRemuve, queueBtnToRemuve, remuveBtnToWatched, remuveBtnToQueue} from './remove-Button';
-const load = key => {
-  try {
-    const serializedState = localStorage.getItem(key);
-    //return serializedState === null ? [] : JSON.parse(serializedState);
-    return (serializedState = JSON.parse(serializedState) || undefined);
-  } catch (error) {
-    console.error('Get state error: ', error.message);
-  }
-};
-
+import {
+  watchedBtnToRemuve,
+  queueBtnToRemuve,
+  remuveBtnToWatched,
+  remuveBtnToQueue,
+} from './remove-Button';
 
 export const storageKeys = {
   WATCHED: 'watched',
   QUEUE: 'queue',
-
 };
 
-export const save = (key, value) => { 
-  try { 
+export const save = (key, value) => {
+  try {
     const serializedState = JSON.stringify(value);
     localStorage.setItem(key, serializedState);
   } catch (error) {
