647f5706cdee06ecbc8d28d9aee5aa631a0ffd70
Vladimir <86652590+Emersonnaw@users.noreply.github.com>
2023-04-13 23:00:48 +0300

    all render

--- src/js/searchByKeyword.js
+++ src/js/searchByKeyword.js
@@ -5,7 +5,6 @@ import searchRenderBox from '../templates/searchRenger.hbs';
 import NewLoader from './loader';
 export { numberOfGeneras, ganreListProcessin };
 
-
 const apiServise = new apiServer();
 const newLoader = new NewLoader();
 const numberOfGeneras = 4;
@@ -40,10 +39,12 @@ async function ganreListProcessin() {
 
 async function createCards(genresBase) {
   try {
+     newLoader.showLoader();
     await new Promise(resolve => setTimeout(resolve, 300));
     await apiServise.fetchSearchMoviesPages().then(({ results }) => {
       refs.wrongSearchMess.classList.add('visually-hidden');
       if (!results.length) {
+        newLoader.hideLoader();
         refs.wrongSearchMess.classList.remove('visually-hidden');
         setTimeout(() => {
           refs.wrongSearchMess.classList.add('visually-hidden');
@@ -55,18 +56,23 @@ async function createCards(genresBase) {
         ({ title, release_date, poster_path, genre_ids, id }) => {
           release_date = release_date.slice(0, 4);
           poster_path = `https://image.tmdb.org/t/p/w500${poster_path}`;
+         
           const genres = [...genresBase.genres];
           const genreIds = genre_ids;
           let genresNames = [];
           const other = 'Other';
+
           for (let i = 0; i < genreIds.length; i++) {
             const genre = genres.find(g => g.id === genreIds[i]);
             genresNames.push(genre.name);
           }
-          let currentGanre = [...genresNames.slice(0, 2), other].join(' ');
-          if (genresNames.length < numberOfGeneras) {
-            currentGanre = [...genresNames].join(' ');
-          }
+
+          let currentGanre = null;
+         if (genresNames.length < numberOfGeneras) {
+               currentGanre = genresNames.join(',  ');
+           } else {
+            currentGanre = [...genresNames.slice(0, 2), other].join(', ');
+           }
           return { title, release_date, currentGanre, poster_path, id };
         }
       );
