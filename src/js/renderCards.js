2b635d5ecc147cb0f29c3102b9e9f5783ac125e3
Eduard <113450719+brugiiii@users.noreply.github.com>
2023-04-14 00:45:00 +0300

    Revert "all render"

--- src/js/renderCards.js
+++ src/js/renderCards.js
@@ -1,44 +1,50 @@
-import cardTemplate from '../templates/cardTemplate.hbs'
+import cardTemplate from '../templates/cardTemplate.hbs';
 import NewApiService from './api-servis';
 import { numberOfGeneras, ganreListProcessin } from './searchByKeyword';
 import NewLoader from './loader';
-const container = document.querySelector('.main-content');
+
 const newApiService = new NewApiService();
 const newLoader = new NewLoader();
+
 ganreListProcessin().then(fetchData);
 
-async function fetchData(genresBase) { 
-  try { 
+async function fetchData(genresBase) {
+  try {
     newLoader.showLoader();
     await new Promise(resolve => setTimeout(resolve, 300));
     await newApiService.fetchPopularMovies().then(({ results }) => {
-      const mk =  results.map(({ poster_path, original_title, genre_ids, release_date, id }) => {
-       
-         const date = release_date.slice(0, 4);
-         const genres = [...genresBase.genres];
-          const genreIds = genre_ids;
-          let genresNames = [];
-          const other = 'Other';
+      results.map(result => {
+        const { poster_path, original_title, genre_ids, release_date, id } =
+          result;
+        const date = release_date.slice(0, 4);
 
-          for (let i = 0; i < genreIds.length; i++) {
-            const genre = genres.find(g => g.id === genreIds[i]);
-            genresNames.push(genre.name);
-          }
+        const genres = [...genresBase.genres];
+        const genreIds = genre_ids;
+        let genresNames = [];
+        const other = 'Other';
+        for (let i = 0; i < genreIds.length; i++) {
+          const genre = genres.find(g => g.id === genreIds[i]);
+          genresNames.push(genre.name);
+        }
+        let currentGanre = [...genresNames.slice(0, 2), other].join(' ');
+        if (genresNames.length < numberOfGeneras) {
+          currentGanre = [...genresNames].join(' ');
+        }
 
-          let currentGanre = null;
-         if (genresNames.length < numberOfGeneras) {
-               currentGanre = genresNames.join(',  ');
-           } else {
-            currentGanre = [...genresNames.slice(0, 2), other].join(', ');
-           }
-        return {poster_path, original_title, currentGanre,date, id};
-       }
-      );
-        const marKup = cardTemplate({mk});
-        container.innerHTML = marKup;
-        newLoader.hideLoader();
-     });
+        const mk = cardTemplate({
+          poster_path,
+          original_title,
+          currentGanre,
+          date,
+          id,
+        });
+
+        const container = document.querySelector('.main-content');
+        container.insertAdjacentHTML('beforeend', mk);
+      });
+    });
+    newLoader.hideLoader();
   } catch (error) {
     console.error('Помилка під час отримання даних:', error);
   }
-} 
+}
