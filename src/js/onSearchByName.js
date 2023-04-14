3f4003c71baee2fdb734f282a2084575507c2c0c
Eduard <113450719+brugiiii@users.noreply.github.com>
2023-04-14 18:31:47 +0300

    Revert "search pagination"

--- src/js/onSearchByName.js
+++ src/js/onSearchByName.js
@@ -1,35 +1,20 @@
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import Notiflix from 'notiflix';
 import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.css';
-// import { totalFilms } from './fetchTrendingMovies';
-// import { pageNumber } from './renderFilms';
-// import { renderMoviesMarkup } from './renderFilms';
+
+import { renderMoviesMarkup } from './renderFilms';
 import { startLoader, stopLoader } from './loader';
 import { refs } from './refs';
 import { MoviesServiceByName } from './apiServiceMovies';
 
+Notiflix.Notify.init({ position: 'center-top' });
 refs.moviesSearchForm.addEventListener('submit', onFormSubmit);
-
-const IMG_URL = 'https://image.tmdb.org/t/p/w500';
-const IMG_URL_RETINA = 'https://image.tmdb.org/t/p/w1280';
-const DEFAULT_POSTER_URL =
-  'https://motivatevalmorgan.com/wp-content/uploads/2016/06/default-movie.jpg';
-function returnGenreName(genres, id) {
-  try {
-    const genreToFind = genres.find(genre => genre.id === id);
-    if (genreToFind) {
-      return genreToFind.name;
-    }
-  } catch (error) {
-    console.log(error);
-  }
-}
 
 const moviesServiceByName = new MoviesServiceByName({
   page: 1,
   searchQuery: '',
 });
-// let pageNumber = 1;
+
 export async function onFormSubmit(e) {
   e.preventDefault();
   startLoader();
@@ -41,7 +26,7 @@ export async function onFormSubmit(e) {
   moviesServiceByName.query = searchQuery;
 
   if (searchQuery === '') {
-    Notify.info('Enter something');
+    Notiflix.Notify.info('Enter something');
     stopLoader();
     return;
   } else {
@@ -62,60 +47,6 @@ export async function onFormSubmit(e) {
       });
     } catch (error) {
       console.log(error);
-    }
-    function renderMoviesMarkup(response) {
-      try {
-        pageNumber = response.data.page;
-
-        const moviesArray = response.data.results;
-        const genres = JSON.parse(localStorage.getItem('genres'));
-        const markup = moviesArray
-          .map(({ poster_path, title, genre_ids, id, release_date }) => {
-            const genresCount = genre_ids.length;
-            const date = release_date.split('').splice(0, 4).join('');
-            let genresToShow = '';
-            const posterUrl = poster_path
-              ? `${IMG_URL}${poster_path}`
-              : DEFAULT_POSTER_URL;
-
-            const srcsetChecked = poster_path
-              ? `${IMG_URL}${poster_path} 1x, ${IMG_URL_RETINA}${poster_path} 2x`
-              : DEFAULT_POSTER_URL;
-
-            if (genresCount === 1) {
-              genresToShow = returnGenreName(genres, genre_ids[0]);
-            } else if (genresCount === 2) {
-              genresToShow = `${returnGenreName(
-                genres,
-                genre_ids[0]
-              )}, ${returnGenreName(genres, genre_ids[1])}`;
-            } else if (genresCount > 2) {
-              genresToShow = `${returnGenreName(
-                genres,
-                genre_ids[0]
-              )}, ${returnGenreName(genres, genre_ids[1])}, Other`;
-            }
-
-            return `<li class="film-card" >
-      <a href="modal-film.html" class="film-card__link" >
-        <div class="film-card__img">
-          <img src="${posterUrl}" alt="${title}" srcset="${srcsetChecked}" loading="lazy" data-id="${id}"/>
-        </div>
-        <div class="film-card__info">
-          <p class="film-card__title">${title}</p>
-          <p class="film-card__description">${genresToShow} | ${date}</p>
-        </div>
-      </a>
-    </li>`;
-          })
-          .join('');
-
-        refs.galleryList.innerHTML = markup;
-
-        stopLoader();
-      } catch (error) {
-        console.log(error);
-      }
     }
   }
 }
